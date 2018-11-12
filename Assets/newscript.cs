dd5561c436ee76bdaf5e13ad2cc1e84b01fcb296
Tianzi Wang <wtianzi@gmail.com>
2018-11-12 15:39:07 -0500

    First commit

---
+++ Assets/newscript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class newscript : MonoBehaviour {
+    public Text[] m_MyText;
+
+    public float speed;
+
+    private Rigidbody rb;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        foreach(Text t_t in m_MyText)
+        {
+            t_t.text = transform.position.ToString();
+        }
+        
+        //Debug.Log("position is: " + spherePosition);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
